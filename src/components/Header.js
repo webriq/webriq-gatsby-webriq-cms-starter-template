fce104e542984cc3304671a765aa6a3236868d76
Allen <allen@webriq.services>
2019-05-08 17:18:10 +0800

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,14 @@ import SideBarMenu from './Sidebar'
 import styl from './header.module.css'
 
 const Header = () => (
-  <header className="bg-primary">
+  <header>
     <Sticky 
       className={styl.stickyWrapper}
-      stickyClassName="is-sticky"
-      stickyStyle={{ transform: 'unset', zIndex: '1' }}
+      stickyClassName={styl.isSticky}
+      stickyStyle={{ transform: 'unset', zIndex: '2' }}
       >
-        <div className="menu-container">
-          <div className="container py-3">
+        <div className={`bg-primary ${styl.menuContainer}`}>
+          <div className="container">
             <div className="row align-items-center">
               <div className="col-6">
                 <Link className="text-white" to="/">LOGO</Link>  
